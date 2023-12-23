395bb2c857e8d2eae83a959f78176c23bae3d7ee
YASH GUPTA <yashgupta5532@gmail.com>
2023-12-23 17:01:03 +0530

    added html files for not creating another repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Citizen from "./FormCard/Citizen.js";
+// import Citizen from "./FormCard/Citizen.js";
 import Citizenship from "./citizenship/Citizenship.js";
 
 function App() {
@@ -8,7 +8,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Citizenship />} />
-        <Route path="/new" element={<Citizen />} />
       </Routes>
     </Router>
   );
